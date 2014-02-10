160b28b58b250f53fa33e277b30c1e4bd1b2fefb
Justin Lange <julange@gmail.com>
2014-02-09 19:30:25 -0500

    animation of blind working

--- js/upblind.js
+++ js/upblind.js
@@ -2,12 +2,17 @@ var margin;
 var blindPos;
 var bottomPos;
 var topPos;
+var blindDown;
+var animationAmount;
 
 function setup() {
   createGraphics(width, height);
   margin = 100;
   bottomPos = height-margin;
   blindPos = 200;
+  blindDown = false;
+  animationAmount = 10;
+  setInterval(animateBlind, 100);
 }
 
 function draw() {
@@ -17,23 +22,36 @@ function draw() {
  	 rect(margin, 100, width-margin, blindPos);
 }
 
-function testInput(){
-  setInterval(lowerBlind, 100);
+function animateBlind(){
+	if(lowering){
+		if(blindPos < bottomPos){
+				blindPos = blindPos+animationAmount;
+			}else{
+				lowering = false;	
+		}
+	}
+		if(raising){
+			if(blindPos > margin){
+				blindPos = blindPos-animationAmount;
+			}else{
+				raising = false;
+			}
+		}
+	}
+
+
+
+
+function lowerBlinds(){
+  raising = false;	
+  lowering = true;
 }
 
 
-function lowerBlind(){
 
-
-if(blindPos < bottomPos){
-blindPos = blindPos+10;
-
-}
-}
-
-function raiseBlind(){
-
-	
+function raiseBlinds(){
+  raising = true;	
+  lowering = false;
 }
 
 
